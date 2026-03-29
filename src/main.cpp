d0862488539a1637cdf0ccccf12495f13b1f28a9
Jake Martin <jake_martin_@outlook.com>
2026-03-29 19:18:40 +0100

    Increase WiFi connection attempts

--- src/main.cpp
+++ src/main.cpp
@@ -104,7 +104,7 @@ bool connectWifi() {
     Serial.printf("Trying WiFi: %s\n", credentials[i].ssid.c_str());
     WiFi.begin(credentials[i].ssid.c_str(), credentials[i].password.c_str());
     int attempts = 0;
-    while (WiFi.status() != WL_CONNECTED && attempts < 3) {
+    while (WiFi.status() != WL_CONNECTED && attempts < 5) {
       delay(500);
       Serial.print(".");
       attempts++;
